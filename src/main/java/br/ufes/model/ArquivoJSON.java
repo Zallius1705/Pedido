09c8672a0e6984fdc09ea43e599e61172c621e73
João Paulo <>
2020-10-07 08:29:51 -0300

    Inserção das classes arquivo

---
+++ src/main/java/br/ufes/model/ArquivoJSON.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufes.model;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author João Paulo
+ */
+public class ArquivoJSON implements IArquivo{
+    
+    //private NotaFiscal nota;
+
+    @Override
+    public void geraNota(NotaFiscal nota) {
+        
+        FileWriter arquivo = null;
+        JSONObject objetoJson = new JSONObject();
+        
+        objetoJson.put("teste", 4);
+        objetoJson.put("teste2", "asdlfja");
+        
+        try {
+            arquivo = new FileWriter("NotaFiscal.json");
+            arquivo.write(objetoJson.toString());
+            arquivo.close();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ArquivoJSON.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        System.out.println(objetoJson.toString());
+        
+        
+    }
+
+    
+  
+}
---
+++ src/main/java/br/ufes/model/ArquivoPDF.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufes.model;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author João Paulo
+ */
+public class ArquivoPDF implements IArquivo{
+    
+    
+
+    @Override
+    public void geraNota(NotaFiscal nota) {
+        
+        Document documento = new Document();
+        
+        try {
+            PdfWriter.getInstance(documento, new FileOutputStream("NotaFiscal.pdf"));
+            documento.open();
+            documento.add(new Paragraph("asdfaldsfçj pdf"));
+            documento.close();
+            
+        } catch (FileNotFoundException | DocumentException ex) {
+            Logger.getLogger(ArquivoPDF.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        System.out.println("PDF gerado!");
+        
+        
+    }
+    
+}
+
